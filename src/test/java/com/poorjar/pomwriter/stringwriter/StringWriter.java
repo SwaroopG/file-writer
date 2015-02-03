f77a000d8310d85cd68f1c97f3b1b897b2c3835e
SwaroopG <swarooprajg@gmail.com>
2015-02-03 11:26:44 -0600

    Added an additional check

--- src/test/java/com/poorjar/pomwriter/stringwriter/StringWriter.java
+++ src/test/java/com/poorjar/pomwriter/stringwriter/StringWriter.java
@@ -15,11 +15,20 @@ public final class StringWriter {
 
         File file = new File(classLoader.getResource("lineups.txt").getPath());
         Scanner input = new Scanner(file);
+        String previousToken = "";
 
         while (input.hasNext()) {
             String nextToken = input.nextLine().trim();
             StringTokenizer tokenizer = new StringTokenizer(nextToken, ",");
-            System.out.println(tokenizer.nextToken() + ".X|pkg00@"+ tokenizer.nextToken() +".ROVI");
+            
+            if (previousToken.equals(nextToken)) {
+                System.out.println("**********" + tokenizer.nextToken() + ".X|pkg00@" + tokenizer.nextToken() + ".ROVI");
+            }
+            
+            if (!previousToken.equals(nextToken)) {
+                System.out.println(tokenizer.nextToken() + ".X|pkg00@" + tokenizer.nextToken() + ".ROVI");
+            }
+            previousToken = nextToken;
         }
 
         input.close();
